b1dc49c42ea27c334917e33616ce2a8476d2089a
JoTzu <ra92392@gmail.com>
2018-07-02 14:29:32 +0800

    nav 第1版

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavComponent } from './component/nav/nav.component';
 
 @NgModule({
   declarations: [
